825bcde70f233ae3c0d7ab12d9088da5bc3651c1
ryanotron <ryan.arya.pratama@gmail.com>
2012-09-10 15:58:53 +0800

    done for today

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from google.appengine.ext import db
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinjaenv = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
+class ActivityModel(db.Model):
+    name = db.StringProperty(required = True)
+    start = db.DateTimeProperty(required = True)
+    end = db.DateTimeProperty(required = True)
+
 def render_str(template, **params):
     t = jinjaenv.get_template(template)
     return t.render(params)
@@ -25,6 +30,14 @@ class MainPageHandler(SuperHandler):
         if not name:
             name = 'anon'
         self.render('mainpage.html', name = name)
+        
+class PanelHandler(SuperHandler):
+    def get(self):
+        self.render('panelpage.html')
+        
+    def post(self):
+        self.render('panelpage.html')
 
-app = webapp2.WSGIApplication([('/', MainPageHandler),],
+app = webapp2.WSGIApplication([('/', MainPageHandler),
+                               ('/panel/?', PanelHandler)],
                               debug = True)
